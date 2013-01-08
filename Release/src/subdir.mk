071b05b8feb7ce8a6debfcc15225dc2a29f67a86
Thierry Blais-Brossard <thierry@blais-brossard.com>
2013-01-07 21:13:05 -0500

    Add some makefile generated by eclipse

--- Release/src/subdir.mk
+++ Release/src/subdir.mk
@@ -5,14 +5,17 @@
 # Add inputs and outputs from these tool invocations to the build variables 
 CPP_SRCS += \
 ../src/USBPanicButton.cpp \
+../src/UsbDevice.cpp \
 ../src/main.cpp 
 
 OBJS += \
 ./src/USBPanicButton.o \
+./src/UsbDevice.o \
 ./src/main.o 
 
 CPP_DEPS += \
 ./src/USBPanicButton.d \
+./src/UsbDevice.d \
 ./src/main.d 
 
 
@@ -20,7 +23,7 @@ CPP_DEPS += \
 src/%.o: ../src/%.cpp
 	@echo 'Building file: $<'
 	@echo 'Invoking: GCC C++ Compiler'
-	g++ -I/usr/include -O3 -Wall -c -fmessage-length=0 -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
+	g++ -I/usr/local/include -O3 -Wall -c -fmessage-length=0 -v -fPIC -MMD -MP -MF"$(@:%.o=%.d)" -MT"$(@:%.o=%.d)" -o "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
