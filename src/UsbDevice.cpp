cef8d0b352f91c2cf2f0def9bf71611b05fd2728
Thierry Blais-Brossard <thierry@blais-brossard.com>
2013-01-09 18:27:20 -0500

    Fix bug : when run code without the device connected.

--- src/UsbDevice.cpp
+++ src/UsbDevice.cpp
@@ -17,11 +17,17 @@ UsbDevice::~UsbDevice() {
 UsbDevice* UsbDevice::createFromConnectedDevice(u_int16_t vendorID,
 		u_int16_t productID) {
 	libusb_init(NULL);
-	libusb_device_handle *targetDeviceHandle = libusb_open_device_with_vid_pid(NULL, vendorID, productID);
+	libusb_device_handle *targetDeviceHandle = libusb_open_device_with_vid_pid(
+			NULL, vendorID, productID);
+
+	if (targetDeviceHandle == NULL)
+		return NULL;
+
 	if (libusb_kernel_driver_active(targetDeviceHandle, 0) > 0)
 		libusb_detach_kernel_driver(targetDeviceHandle, 0);
+
 	libusb_claim_interface(targetDeviceHandle, 0);
-	return targetDeviceHandle != NULL ? new UsbDevice(targetDeviceHandle) : NULL;
+	return new UsbDevice(targetDeviceHandle);
 }
 
 bool UsbDevice::isConnected() {
@@ -30,8 +36,8 @@ bool UsbDevice::isConnected() {
 
 bool UsbDevice::sendControlMsg(int rquestType, int request, int wValue,
 		int wIndex, int timeout, unsigned char* data, int dataSize) {
-	int result = libusb_control_transfer(deviceHandle, rquestType, request, wValue,
-			wIndex, data, dataSize, timeout);
+	int result = libusb_control_transfer(deviceHandle, rquestType, request,
+			wValue, wIndex, data, dataSize, timeout);
 
 	if (result == -6) {
 		deviceHandle = NULL;
@@ -43,7 +49,7 @@ bool UsbDevice::sendControlMsg(int rquestType, int request, int wValue,
 void UsbDevice::readData(unsigned char *data, int size, int timeout) {
 	int transferred;
 	libusb_interrupt_transfer(deviceHandle, LIBUSB_ENDPOINT_IN | 1, data, size,
-		&transferred, timeout);
+			&transferred, timeout);
 }
 
 void UsbDevice::close() {
